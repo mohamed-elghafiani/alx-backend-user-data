ed18d95b8c0ea7bdc4d63e6cdacd4fb84cbe2c4a
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-04-11 06:45:29 -0700

    added typing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,7 +13,8 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message:str, separator:str) -> str:
+def filter_datum(
+      fields: List[str], redaction: str, message: str, separator: str) -> str:
     """Obfuscates specified fields in the log message"""
     for field in fields:
         message = re.sub(
