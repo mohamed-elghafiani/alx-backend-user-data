9637018d6e910147ae492e0595a78c7d8e7e268d
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-04-11 08:10:57 -0700

    Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -35,7 +35,17 @@ def get_logger() -> logging.Logger:
     """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
-    logger.StreamHandler(RedactingFormatter)
+    logger.propagate = False
+
+    # Create a StreamHandler
+    stream_handler = StreamHandler()
+
+    # Set the formatter to RedactingFormatter with PII_FIELDS
+    formatter = RedactingFormatter(PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+
+    # Add the StreamHandler to the logger
+    logger.addHandler(stream_handler)
 
     return logger
 
