5545f2b05084000446572b64cd7101ac1b704b8c
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-04-12 06:41:16 -0700

    Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -14,7 +14,7 @@ def hash_password(password: str) -> bytes:
     return hashed
 
 
-def is_valid(hashed_password, password):
+def is_valid(hashed_password: bytes, password: str) -> bool:
     """Check valid password
     """
     return bcrypt.checkpw(password.encode(), hashed_password)
