7da2168ed5bfe298bf00c8811e21bb7fefb5fb31
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-04-17 11:36:58 -0700

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """Basic Auth module
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -25,3 +26,24 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header.split(" ")[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """returns the decoded value of a Base64 string
+           base64_authorization_header:
+        Returns:
+        - None if base64_authorization_header is None
+        - None if base64_authorization_header is not a string
+        - None if base64_authorization_header is not a valid Base64
+        - Otherwise, return the decoded value as UTF8 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            auth_bytes = base64.b64decode(base64_authorization_header)
+            return auth_bytes.decode('utf-8')
+        except (UnicodeDecodeError, TypeError):
+            return None
