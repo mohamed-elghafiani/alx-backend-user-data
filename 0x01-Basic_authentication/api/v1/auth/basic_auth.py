bcfa2548d3373eb5605f85815bf232d6c5a6d256
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-04-17 10:33:45 -0700

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -21,7 +21,7 @@ class BasicAuth(Auth):
         """
         if authorization_header is None:
             return None
-        if isinstance(authorization_header, str):
+        if not isinstance(authorization_header, str):
             return None
         if authorization_header.split(" ")[0] != "Basic":
             return None
