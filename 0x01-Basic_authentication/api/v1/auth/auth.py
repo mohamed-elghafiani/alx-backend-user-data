e85d7544898a6e45cfdfe4d0e91c1009f6f7800e
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-04-17 05:49:24 -0700

    Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Auth Module
+"""
+from flask import request
+
+
+class Auth:
+    """Auth class to manage the API authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Public method
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Public Method
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Public method
+        """
+        return None
