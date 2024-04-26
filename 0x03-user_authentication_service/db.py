39bfef3df475105ff7ab59cc41cd09373067c713
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-04-26 09:23:47 -0700

    find user with kwargs

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError, NoResultFound
+from sqlalchemy.orm.exc import InvalidRequestError, NoResultFound
 
 from user import Base, User
 
