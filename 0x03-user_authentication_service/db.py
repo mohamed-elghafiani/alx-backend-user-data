efe9c0e3af62b1d1ab240afd134e97010e655180
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-04-26 09:39:09 -0700

    find user with kwargs

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import InvalidRequestError, NoResultFound
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -47,7 +48,10 @@ class DB:
         """
         _ = self._session
 
-        user = self.__session.query(User).filter_by(**kwarg).first()
-        if not user:
-            raise NoResultFound("User not found")
-        return user
+        try:
+           user = self.__session.query(User).filter_by(**kwarg).first()
+           if not user:
+               raise NoResultFound
+           return user
+       except InvalidRequestError as e:
+           raise e
