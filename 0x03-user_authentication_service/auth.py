f36d6fc4943491d23ec4bf781a18a10c664415ad
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-04-27 08:28:10 -0700

    added create_session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -52,3 +52,14 @@ class Auth:
             return False
         except (NoResultFound, InvalidRequestError):
             return False
+
+    def create_session(self, email: str) -> str:
+        """add a session_id to user with @email
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except (NoResultFound, InvalidRequestError):
+            return None
