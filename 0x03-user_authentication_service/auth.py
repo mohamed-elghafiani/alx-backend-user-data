97da192b3eca29f56a09ea282ee1e8bb6af3abba
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-04-27 10:06:57 -0700

    Find user by session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -63,3 +63,13 @@ class Auth:
             return session_id
         except (NoResultFound, InvalidRequestError):
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Takes single session_id string argument
+        Returns corresponding User or None
+        """
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
