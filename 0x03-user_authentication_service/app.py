67887b4bb5220d35751d0ae9e0d05fe3df749b3a
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-04-30 10:30:28 -0700

    added /profile endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -69,6 +69,8 @@ def profile() -> str:
         - The user's profile information.
     """
     session_id = request.cookies.get("session_id")
+    if not session_id:
+        abort(403)
     user = AUTH.get_user_from_session_id(session_id)
     if user is None:
         abort(403)
