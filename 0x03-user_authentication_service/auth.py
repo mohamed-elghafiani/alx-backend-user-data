d79d8d57e726fc050b16827896aedb1789de2c84
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-04-30 10:04:57 -0700

    removed print

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -83,7 +83,6 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(id=user_id)
-            print(user.id)
             self._db.update_user(user.id, session_id=None)
         except NoResultFound:
             return None
