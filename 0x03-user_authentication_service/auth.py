e815208a404baa48aea48c8b6f5a5b96da659920
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-04-30 10:38:17 -0700

    implement the Auth.get_reset_password_token method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -94,8 +94,6 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
-            user = None
-        if user is None:
             raise ValueError()
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
